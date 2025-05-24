9ede9999a139f855662bb73250c85700ccff94ca
Antooon32 <robotkartoshka@gmail.com>
2025-05-24 21:53:17 +0300

    svg hover

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+    // Не працює
+//     const hearticon = document.querySelector('heart');
+//     const heartbackground = document.querySelector('action-icon');
+
+//     if (heartbackground) {
+//     heartbackground.addEventListener('click', () => {
+//         heartbackground.classList.toggle('activ-icon');
+//         hearticon.classList.toggle('activ-heart');
+//     });
+//   }
+
+
+// });
